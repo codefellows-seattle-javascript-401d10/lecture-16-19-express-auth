08f6452aab417c9ebe9b8bdfa8a71fecc69d2c73
raziyeh bazargan <r.bazargan@gmail.com>
2016-10-03 14:10:57 -0700

    initial

---
+++ lab-raziyeh/server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//npm modules
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const express = require('express');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const debug = require('debug')('slug:server');
+
+//app modules
+const errorMiddleware = require('./lib/error-middleware.js');
+const basicErrorMiddleware = require('./lib/basic-auth-middleware.js');
+
+const authRouther = require('./route/auth-router.js');
+
+//load env vars
+dotenv.load();
+
+//setup mongoose
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+//module constants
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.user(authRouther);
+app.use(errorMiddleware);
+app.use(basicErrorMiddleware);
+
+const server = module.exports = app.listen(PORT, () => {
+  debug(`server is up ${PORT}`);
+});
+
+server.isRunning = true;
