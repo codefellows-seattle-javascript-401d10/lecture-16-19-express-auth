d277ca0c522faebaa7bcff13ab889b7199d1d8b0
meeksfred <maxfred0025@gmail.com>
2016-10-03 14:38:23 -0700

    user model working, tests passing

---
+++ lab-max/test/auth-router-test.js
@@ -0,0 +1,154 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const User = require('../model/user.js');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const server = require('../server.js');
+const debug = require('debug')('meeksgram:auth-router-test');
+const PORT = process.env.PORT || 3000;
+
+mongoose.promise = Promise;
+
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  username: 'meeks',
+  password: '12345',
+  email: 'meeks@meeks.com',
+};
+
+describe('testing auth-router', function(){
+
+  before((done) => {
+    debug('before module store-router-test');
+    if (! server.isRunning) {
+      server.listen(PORT, () => {
+        server.isRunning = true;
+        debug(`server up ::: ${PORT}`);
+        done();
+      });
+      return;
+    }
+    done();
+  });
+
+  after((done) => {
+    debug('after module store-router-test');
+    if (server.isRunning) {
+      server.close(() => {
+        server.isRunning = false;
+        debug('server down');
+        done();
+      });
+      return;
+    }
+    done();
+  });
+
+  describe('testing POST /api/signup', function(){
+    describe('with valid body', function(){
+
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(!!res.text).to.equal(true);
+          done();
+        });
+      });
+    });
+
+    describe('with invalid body', function(){
+
+      it('should respond with a 400 validation error', done => {
+        let badUser = {
+          username: 5,
+          password: 'house',
+          email: '',
+        };
+        request.post(`${url}/api/signup`)
+        .send(badUser)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(!!res.text).to.equal(true);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('testing GET /api/signup', function(){
+    describe('with valid body', function(){
+
+      before( done => {
+        let user = new User(exampleUser);
+        user.generatePasswordHash(exampleUser.password)
+        .then(user => user.save())
+        .then(user => {
+          this.tempUser = user;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.get(`${url}/api/login`)
+        .auth('meeks', '12345')
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(!!res.text).to.equal(true);
+          done();
+        });
+      });
+    });
+
+    describe('with invalid body', function(){
+
+      before( done => {
+        let user = new User(exampleUser);
+        user.generatePasswordHash(exampleUser.password)
+        .then(user => user.save())
+        .then(user => {
+          this.tempUser = user;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+
+      it('should return 401 error for user not being authenticated/unauthorized', done => {
+        request.get(`${url}/api/login`)
+        .auth('meeks', '99999')
+        .end((err, res) => {
+          console.log('res-text',res.text);
+          expect(res.status).to.equal(401);
+          expect(!!res.text).to.equal(true);
+          done();
+        });
+      });
+    });
+  });
+});
