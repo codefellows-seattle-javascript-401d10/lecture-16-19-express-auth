c44176b0682b76ec913a17cb38d2eca4538136bc
Lee Broxson <broxsonl@gmail.com>
2016-10-04 14:57:07 -0700

    PUT routes written.

--- lab-lee/test/gallery-router.test.js
+++ lab-lee/test/gallery-router.test.js
@@ -93,6 +93,7 @@ describe('Testing /api/gallery routes', function() {
       })
       .end((err, res) => {
         if (err) return done(err);
+        expect(res.status).to.equal(200);
         expect(res.body.name).to.equal(exampleGallery.name);
         expect(res.body.desc).to.equal(exampleGallery.desc);
         expect(res.body.userID).to.equal(this.tempUser._id.toString());
@@ -156,7 +157,7 @@ describe('Testing /api/gallery routes', function() {
       delete exampleGallery.userID;
     });
 
-    it('no token, so should return unauthorized and status 401', done => {
+    it('valid token, so should return gallery and status 200', done => {
 
       request.get(`${url}/api/gallery/${this.tempGallery._id}`)
       .set({
@@ -164,6 +165,7 @@ describe('Testing /api/gallery routes', function() {
       })
       .end((err, res) => {
         if (err) return done(err);
+        expect(res.status).to.equal(200);
         expect(res.body.name).to.equal(exampleGallery.name);
         expect(res.body.desc).to.equal(exampleGallery.desc);
         expect(res.body.userID).to.equal(this.tempUser._id.toString());
@@ -182,9 +184,95 @@ describe('Testing /api/gallery routes', function() {
       });
     });
 
-    it('no body, so should return bad request and status 404', done => {
+    it('invalid id, so should return not found and status 404', done => {
 
       request.get(`${url}/api/gallery/`)
+      .set({
+        'Authorization': `Bearer ${this.tempToken}`,
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('testing PUT to /api/gallery/:id', () => {
+
+    before( done => {
+      new User(exampleUser)
+      .generatePasswordHash(exampleUser.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    before( done => {
+      exampleGallery.userID = this.tempUser._id.toString();
+      new Gallery(exampleGallery).save()
+      .then( gallery => {
+        this.tempGallery = gallery;
+        done();
+      })
+      .catch(done);
+    });
+
+    after(() => {
+      delete exampleGallery.userID;
+    });
+
+    it('should update an existing gallery by ID and status 200', done => {
+      let updateData = {name:'bob'};
+      request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+      .send(updateData)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`,
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal('bob');
+        expect(res.body.desc).to.equal(exampleGallery.desc);
+        expect(res.body.userID).to.equal(this.tempUser._id.toString());
+        let date = new Date(res.body.created).toString();
+        expect(date).to.not.equal('Invalid Date');
+        done();
+      });
+    });
+
+    it('no token, so should return unauthorized and status 401', done => {
+      request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+      .send(exampleGallery)
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      });
+    });
+
+    it('no body, so should return bad request and status 400', done => {
+      request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+      .set('Content-Type', 'application/json')
+      .send('notjson')
+      .set({
+        'Authorization': `Bearer ${this.tempToken}`,
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+
+    it('invalid id, so should return not found and status 404', done => {
+
+      request.put(`${url}/api/gallery/`)
+      .send(exampleGallery)
       .set({
         'Authorization': `Bearer ${this.tempToken}`,
       })
