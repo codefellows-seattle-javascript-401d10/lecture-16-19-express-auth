41ba2030f87af889bee714948cf50748f427ac9c
JudyVue <judyvue87@gmail.com>
2016-10-04 17:00:49 -0700

    indendations suck

--- lab-judy/test/gallery-router-test.js
+++ lab-judy/test/gallery-router-test.js
@@ -125,13 +125,13 @@ describe('test /api/gallery', function(){
         done();
       });
     });
-  });
+
 
 //GET tests
-  describe('testing GET to /api/gallery/:id', () => {
-    before(done => {
-      console.log('create user');
-      new User(exampleUser)
+    describe('testing GET to /api/gallery/:id', () => {
+      before(done => {
+        console.log('create user');
+        new User(exampleUser)
       .generatePasswordHash(exampleUser.password)
       .then( user => user.save())
       .then( user => {
@@ -143,26 +143,26 @@ describe('test /api/gallery', function(){
         done();
       })
       .catch(done);
-    });
+      });
 
-    before( done => {
-      console.log('create gallery');
-      exampleGallery.userID = this.tempUser._id.toString();
-      new Gallery(exampleGallery).save()
+      before( done => {
+        console.log('create gallery');
+        exampleGallery.userID = this.tempUser._id.toString();
+        new Gallery(exampleGallery).save()
       .then( gallery => {
         this.tempGallery = gallery;
         done();
       })
       .catch(done);
-    });
+      });
 
-    after(() => {
-      delete exampleGallery.userID;
-    });
+      after(() => {
+        delete exampleGallery.userID;
+      });
 
     //GET test 200 to return a gallery
-    it('should return a gallery and status 200', done => {
-      request.get(`${url}/api/gallery/${this.tempGallery._id}`)
+      it('should return a gallery and status 200', done => {
+        request.get(`${url}/api/gallery/${this.tempGallery._id}`)
       .set({
         Authorization: `Bearer ${this.tempToken}`
       })
@@ -176,32 +176,32 @@ describe('test /api/gallery', function(){
         expect(date).to.not.equal('Invalid Date');
         done();
       });
-    });
+      });
 
     //GET test 401 for unauthorized token or no token
-    it('should return status 401 for unauthorized token or no token given', (done) => {
-      request.get(`${url}/api/gallery/${this.tempGallery._id}`)
+      it('should return status 401 for unauthorized token or no token given', (done) => {
+        request.get(`${url}/api/gallery/${this.tempGallery._id}`)
         .end((err, res) => {
           expect(res.status).to.equal(401);
           done();
         });
-    });
+      });
     //GET test 404 for bad request with invalid id
-    it('should return 404 for id not found', (done) => {
-      request.get(`${url}/api/gallery/`)
+      it('should return 404 for id not found', (done) => {
+        request.get(`${url}/api/gallery/`)
       .set({Authorization: `Bearer ${this.tempToken}`})
       .end((err, res) => {
         expect(res.status).to.equal(404);
         done();
       });
-    });
-  });
+      });
+
 
 //PUT tests
-  describe('test PUT to /api/gallery/:id', () => {
-    before(done => {
-      console.log('create user');
-      new User(exampleUser)
+      describe('test PUT to /api/gallery/:id', () => {
+        before(done => {
+          console.log('create user');
+          new User(exampleUser)
       .generatePasswordHash(exampleUser.password)
       .then( user => user.save())
       .then( user => {
@@ -213,27 +213,27 @@ describe('test /api/gallery', function(){
         done();
       })
       .catch(done);
-    });
+        });
 
-    before( done => {
-      console.log('create gallery');
-      exampleGallery.userID = this.tempUser._id.toString();
-      new Gallery(exampleGallery).save()
+        before( done => {
+          console.log('create gallery');
+          exampleGallery.userID = this.tempUser._id.toString();
+          new Gallery(exampleGallery).save()
       .then( gallery => {
         this.tempGallery = gallery;
         done();
       })
       .catch(done);
-    });
+        });
 
-    after(() => {
-      delete exampleGallery.userID;
-    });
+        after(() => {
+          delete exampleGallery.userID;
+        });
 
     //PUT test 200 for updated gallery properly returned
-    it('should update gallery and return it with status 200', (done) => {
-      debug('testing PUT request for status 200');
-      request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+        it('should update gallery and return it with status 200', (done) => {
+          debug('testing PUT request for status 200');
+          request.put(`${url}/api/gallery/${this.tempGallery._id}`)
       .set({
         Authorization: `Bearer ${this.tempToken}`
       })
@@ -245,43 +245,11 @@ describe('test /api/gallery', function(){
         expect(res.body.desc).to.equal('really sunny');
         done();
       });
-    });
-  });
-  describe('testing PUT test to /api/gallery/:id', () => {
-    before(done => {
-      console.log('create user');
-      new User(exampleUser)
-      .generatePasswordHash(exampleUser.password)
-      .then( user => user.save())
-      .then( user => {
-        this.tempUser = user;
-        return user.generateToken();
-      })
-      .then( token => {
-        this.tempToken = token;
-        done();
-      })
-      .catch(done);
-    });
-
-    before( done => {
-      console.log('create gallery');
-      exampleGallery.userID = this.tempUser._id.toString();
-      new Gallery(exampleGallery).save()
-      .then( gallery => {
-        this.tempGallery = gallery;
-        done();
-      })
-      .catch(done);
-    });
-
-    after(() => {
-      delete exampleGallery.userID;
-    });
+        });
 
     //PUT test 400 for passing invalid body
-    it('should return status 400 for passing in invalid body', (done) => {
-      request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+        it('should return status 400 for passing in invalid body', (done) => {
+          request.put(`${url}/api/gallery/${this.tempGallery._id}`)
       .set('Content-type', 'application/json')
       .set({Authorization: `Bearer ${this.tempToken}`})
       .send('{')
@@ -289,97 +257,33 @@ describe('test /api/gallery', function(){
         expect(res.status).to.equal(400);
         done();
       });
-    });
-  });
-  describe('testing PUT request', () => {
-    before(done => {
-      console.log('create user');
-      new User(exampleUser)
-      .generatePasswordHash(exampleUser.password)
-      .then( user => user.save())
-      .then( user => {
-        this.tempUser = user;
-        return user.generateToken();
-      })
-      .then( token => {
-        this.tempToken = token;
-        done();
-      })
-      .catch(done);
-    });
-
-    before( done => {
-      console.log('create gallery');
-      exampleGallery.userID = this.tempUser._id.toString();
-      new Gallery(exampleGallery).save()
-      .then( gallery => {
-        this.tempGallery = gallery;
-        done();
-      })
-      .catch(done);
-    });
-
-    after(() => {
-      delete exampleGallery.userID;
-    });
+        });
+      });
 
     //PUT test 401 response w/ 'unauthorized' if bad token given
-    it('should return status 401 and "unauthorized" for bad token or no token given', done => {
-      request.put(`${url}/api/gallery/${this.tempGallery._id}`)
+      it('should return status 401 and "unauthorized" for bad token or no token given', done => {
+        request.put(`${url}/api/gallery/${this.tempGallery._id}`)
       .send({name: 'new beach', desc:'really sunny'})
       .end((err, res) => {
         expect(res.status).to.equal(401);
         done();
       });
-    });
-  });
-  describe('testing PUT route to /api/gallery/:id', () => {
-    before(done => {
-      console.log('create user');
-      new User(exampleUser)
-      .generatePasswordHash(exampleUser.password)
-      .then( user => user.save())
-      .then( user => {
-        this.tempUser = user;
-        return user.generateToken();
-      })
-      .then( token => {
-        this.tempToken = token;
-        done();
-      })
-      .catch(done);
-    });
+      });
 
-    before( done => {
-      console.log('create gallery');
-      exampleGallery.userID = this.tempUser._id.toString();
-      new Gallery(exampleGallery).save()
-      .then( gallery => {
-        this.tempGallery = gallery;
-        done();
-      })
-      .catch(done);
-    });
-
-    after(() => {
-      delete exampleGallery.userID;
-    });
 
     //PUT test 404 for bad id given
-    it('should return status 404 for bad id given', (done) => {
-      request.put(`${url}/api/gallery/badid#`)
+      it('should return status 404 for bad id given', (done) => {
+        request.put(`${url}/api/gallery/badid#`)
       .set({Authorization: `Bearer ${this.tempToken}`})
       .end((err, res) => {
         expect(res.status).to.equal(404);
         done();
       });
-    });
-  });
-
-  describe('testing DELETE routes', function(){
-    before(done => {
-      console.log('create user');
-      new User(exampleUser)
+      });
+      describe('testing DELETE routes', function(){
+        before(done => {
+          console.log('create user');
+          new User(exampleUser)
       .generatePasswordHash(exampleUser.password)
       .then( user => user.save())
       .then( user => {
@@ -391,41 +295,43 @@ describe('test /api/gallery', function(){
         done();
       })
       .catch(done);
-    });
+        });
 
-    before( done => {
-      console.log('create gallery');
-      exampleGallery.userID = this.tempUser._id.toString();
-      new Gallery(exampleGallery).save()
+        before( done => {
+          console.log('create gallery');
+          exampleGallery.userID = this.tempUser._id.toString();
+          new Gallery(exampleGallery).save()
       .then( gallery => {
         this.tempGallery = gallery;
         done();
       })
       .catch(done);
-    });
+        });
 
-    after(() => {
-      delete exampleGallery.userID;
-    });
+        after(() => {
+          delete exampleGallery.userID;
+        });
 
     //Test DELETE for status 204
-    it('should return status 204 for successful deletion', (done) => {
-      request.delete(`${url}/api/gallery/${this.tempGallery._id}`)
+        it('should return status 204 for successful deletion', (done) => {
+          request.delete(`${url}/api/gallery/${this.tempGallery._id}`)
       .set({Authorization: `Bearer ${this.tempToken}`})
       .end((err, res) => {
         if (err) return done (err);
         expect(res.status).to.equal(204);
         done();
       });
-    });
+        });
 
     //Test DELETE for status 404 for bad route
-    it('should return 404 for route not found', (done) => {
-      request.delete(`${url}/api/gallery/badid#`)
+        it('should return 404 for route not found', (done) => {
+          request.delete(`${url}/api/gallery/badid#`)
       .set({Authorization: `Bearer ${this.tempToken}`})
       .end((err, res) => {
         expect(res.status).to.equal(404);
         done();
+      });
+        });
       });
     });
   });
